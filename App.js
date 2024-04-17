6546e9d192e06d4e1a8ada56b1b82384c1964458
Trevor Garnett <tjg0020@uah.edu>
2024-04-17 17:25:21 -0500

    First Push – created graphs and colored them the way I liked.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import CardView from './CardView';
+import LineChart_CV from './CardView';
 
 const yAxis = [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]]; // Two lines
 const xAxis = ["Jan", "Feb", "Mar", "Apr", "May"];
@@ -8,7 +8,8 @@ const xAxis = ["Jan", "Feb", "Mar", "Apr", "May"];
 export default function App() {
   return (
     <View style={styles.container}>
-      <CardView xAxis={["a", "b", "c"]} yAxis={[[1, 2, 3], [2, 1, 3]]} />
+      <LineChart_CV xValues={["a", "b", "c"]} yValues={[[1, 2, 3], [2, 1, 3]]} />
+      <LineChart_CV xValues={xAxis} yValues={yAxis} />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -18,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- CardView.js
+++ CardView.js
@@ -1,58 +1,60 @@
 import {
     LineChart,
-    BarChart,
-    PieChart,
-    ProgressChart,
-    ContributionGraph,
-    StackedBarChart
+    // BarChart,
+    // PieChart,
+    // ProgressChart,
+    // ContributionGraph,
+    // StackedBarChart
 } from "react-native-chart-kit";
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
 import { Dimensions } from "react-native";
 
-export default function CardView(xValues, yValues, xlabel = "", ylabel = "") {
-    console.log("Done");
-    // let datasets = yValues.map((y) => { data: y }); // Map each line to its own data set
-    console.log("here");
-    console.log(yValues);
-    console.log("issue");
-    console.log(xValues);
-    console.log("after both");
-    let width = Dimensions.get("window").width; // From react-native
+export default function LineChart_CV({ xValues, yValues, xlabel = "", ylabel = "" }) {
+    const datasets = yValues.map((y) => ({ data: y })); // Map each line to its own data set
+    const screenWidth = Dimensions.get("window").width; // From react-native
+    const width = screenWidth * 0.9; // 90% of screen width
+    const height = width / 16 * 9; // 16-9 aspect ratio
+    const decimalPlaces = 0;
+
+    const paddingLeft = Math.max(30, screenWidth * 0.05);
+    const paddingRight = paddingLeft;
     return (
-        <View>
-            <Text>Bezier Line Chart</Text>
-            <LineChart
-                data={{
-                    labels: xValues,
-                    datasets: datasets
-                }}
-                width={width} // from react-native
-                height={width / 16 * 9} // 16-9 aspect ratio
-                yAxisLabel={xlabel}
-                yAxisSuffix={ylabel}
-                yAxisInterval={1} // optional, defaults to 1
-                chartConfig={{
-                    backgroundColor: "#ffffff",
-                    backgroundGradientFrom: "#fb8c00",
-                    backgroundGradientTo: "#ffa726",
-                    decimalPlaces: 2, // optional, defaults to 2dp
-                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                    style: {
-                        borderRadius: 16
-                    },
-                    propsForDots: {
-                        r: "6",
-                        strokeWidth: "2",
-                        stroke: "#ffa726"
-                    }
-                }}
-                bezier
-                style={{
-                    marginVertical: 8,
-                    borderRadius: 16
-                }}
-            />
-        </View>
+        <LineChart
+            data={{
+                labels: xValues,
+                datasets: datasets
+            }}
+            withShadow={false}
+            width={width} // from react-native
+            height={height} // 16-9 aspect ratio
+            yAxisLabel={ylabel}
+            fromNumber={3}
+            yAxisInterval={1} // optional, defaults to 1
+            // formatXLabel={(xValues) => xValues.push("s")}
+            chartConfig={{
+                backgroundColor: "#ffffff",
+                backgroundGradientFrom: "#ffffff",
+                backgroundGradientTo: "#ffffff",
+                decimalPlaces: decimalPlaces, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(51, 156, 242, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                style: {
+                    borderRadius: 16,
+                },
+                propsForDots: {
+                    r: "2",
+                    fill: "#339cf2",
+                    strokeWidth: "2",
+                    stroke: "#339cf2"
+                }
+            }}
+            bezier
+            style={{
+                marginVertical: 8,
+                borderRadius: 8,
+                paddingRight: paddingRight + 4 * decimalPlaces,
+                paddingLeft: paddingLeft + 4 * decimalPlaces
+            }}
+        />
     );
 }
